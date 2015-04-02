156833eda7640b53f252419a66781c744e62fb0b
Tylor Dodge <dodget@wit.edu>
2015-04-02 14:16:11 -0400

    new stuff

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+<title>Message</title>
+</head>
+
+<body>
+
+	<form action="process.php" method="post">
+		Enter your number to receive a totally rad text! +1 <input name="number1" type="text" maxlength="3"><input name="number2" type="text" maxlength="3"><input name="number3" type="text" maxlength="4">
+		<input type="submit">
+	
+	</form>
+
+</body>	
+</html>
